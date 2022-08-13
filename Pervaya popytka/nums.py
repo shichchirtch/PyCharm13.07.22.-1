3aa3176f01f4b2b2f2c9ad1fe27dd0a986156ed1
shichchirtch <ivanvol9l@gmail.com>
2022-08-13 23:43:09 +0400

    Добавлен модуль стрин и функция mult(a,b)

--- Pervaya popytka/nums.py
+++ Pervaya popytka/nums.py
@@ -1,12 +1,18 @@
 def plus(a,b):
     return a+b
 
-text = "new text"
+
 def minus(a,b):
     return a-b
+
+def multy(a,b):
+    return a*b
+
 if __name__ == "__main__":
     print(plus(2, 2))
     print(minus(4, 2))
+    print(multy(2, 7))
+
 
 
 
